b58428802df2044bf115eebc5113460667111e33
Salih <salih58.1999@gmail.com>
2024-04-09 17:48:57 +0200

    test

---
+++ exercise.py
@@ -0,0 +1,24 @@
+class Exercise:
+    name = ""
+    sets = 0
+    reps = 0
+    actualSets = []
+    actualReps = []
+    weights = []
+
+    def __init__(self, name, sets, reps):
+        self.name = name
+        self.sets = sets
+        self.reps = reps
+        self.actualSets
+        self.actualReps
+        self.weights
+    
+
+    def addActualSets():
+        geh = 0
+
+    def addActualReps():
+        
+
+    def addWeights():
---
+++ workout.py
@@ -0,0 +1,14 @@
+from exercise import Exercise
+
+class Workout():
+    name = ""
+    self.exercises = []
+
+    def __init__(self, name):
+        self.name = name
+        
+    def createWorkout():
+
+    def addExercise():
+        self.excercises.append(Exercise)
+    def removeExercise():
